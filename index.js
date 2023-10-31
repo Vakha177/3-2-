5dcc4f29494caf94e9195a58a051507892c6f503
Vakha Gonikeev <sheikh2003@mail.ru>
2023-10-31 10:37:26 +0300

    task3

---
+++ index.js
@@ -0,0 +1,3 @@
+import oriMod from './welcome.js'
+
+console.log(`Привет,юзер.Вижу ты зашел с ${oriMod}`)
---
+++ welcome.js
@@ -0,0 +1,10 @@
+// const os = require('os') ;
+//import os from 'node:os';
+
+///console.log(os) ; 
+
+import os from 'node:os'
+const oriMod = os.type()
+export default oriMod
+
+
